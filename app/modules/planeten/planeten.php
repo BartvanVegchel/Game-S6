062ab95cd759a45cdd3739ee9963dc6b40ed4e1b
Theron <therongaray@gmail.com>
2016-03-29 14:29:07 +0200

    Planeet afbeeldingen
    Planeten

---
+++ app/modules/planeten/planeten.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Planeten</h1>
+
+<img src="../../images/P1.png">
+
+<img src="../../images/P2.png">
+
+<img src="../../images/P3.png">
+
+<img src="../../images/P4.png">
+
+<img src="../../images/P5.png">
+
+<img src="../../images/P6.png">
+
+</body>
+</html>
