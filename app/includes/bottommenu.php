9a74a25c27d8fd14ecc3667f5594c2404c85981f
Fraank92 <Snoeppot1!>
2016-03-31 12:23:31 +0200

    bottom menu

--- app/includes/bottommenu.php
+++ app/includes/bottommenu.php
@@ -1,15 +1,11 @@
 <section class="nav bottom">
-
 	<div class="bottomBtn">
 		<p class="screenUp">Button</p>
 	</div>
-
 	<div class="bottomBtn questmaster">
 		<img src="images/questmaster_still.png">
 	</div>
-
 	<div class="bottomBtn">
 		<p class="screenUp">Button</p>
 	</div>
-
 </section>
