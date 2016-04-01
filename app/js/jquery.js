6868bac40b71590d93251df09b42351516eb0352
BartvanVegchel <bart.vanvegchel@hotmail.com>
2016-04-01 10:21:48 +0200

    Toevoegen map

--- app/js/jquery.js
+++ app/js/jquery.js
@@ -8,4 +8,14 @@ $(document).ready(function() {
         $(".socialScreenUp").animate({top: "100%"});
     });
 
+    //grid maken
+    for (var i = 0; i < 100; i++)
+    {
+        $("section.map").append("<div class='part'><div class='overlay'></div><div class='background'></div></div>");
+    }
+
+    $('.part').click(function(){
+        $(this).find(".overlay").hide();
+    });
+
 });
