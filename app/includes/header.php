86107d2b411a8145d91be79daa46317d8493a687
Fraank92 <Snoeppot1!>
2016-04-01 11:33:15 +0200

    subnav

--- app/includes/header.php
+++ app/includes/header.php
@@ -15,8 +15,8 @@
 <!-- Meta -->            
 <title>Gamification app</title>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	
+<meta name="viewport" content="width=device-width, initial-scale=3.0, maximum-scale=1.0, user-scalable=no" />
+
 <!-- Icons -->
 <link rel="shortcut icon" type="image/x-icon" href="images/icons/favicon.ico">
 
