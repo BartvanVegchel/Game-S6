a3dee3d0bea5cbe199ec1c9b3d11701c74f13b28
Geert Molenaar <geert199@gmail.com>
2016-04-06 11:27:22 +0200

    Check if loggedin op home.php, anders redirect naar index.php

--- app/home.php
+++ app/home.php
@@ -1,11 +1,11 @@
 <? 
-if(isset($_SESSION["logged_in"])){ //controleer of je bent ingelogd
 	include('includes/header.php');
 	include('functions/login_function.php');
 	
 	include 'includes/topmenu.php';
 	include 'includes/bottommenu.php';
-
+	
+if(isset($_SESSION["logged_in"])){ //controleer of je bent ingelogd
 	
 	?>
 
@@ -20,13 +20,11 @@ if(isset($_SESSION["logged_in"])){ //controleer of je bent ingelogd
 		<!-- include the map -->
 		<? include 'modules/map/map.php'; ?>
 
-
 <? 
+}
+else {
+	header("Refresh: 0; url=index.php");
+}
+
 include 'includes/footer.php'; 
-
-
-		}
-	else {
-		header("Refresh: 0; url=index.php");
-	}
 ?>
