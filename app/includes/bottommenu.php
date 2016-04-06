34cbbae9699101780331841fb47384f0fcfb158d
Geert Molenaar <geert199@gmail.com>
2016-04-06 13:35:40 +0200

    Bottommenu changes

--- app/includes/bottommenu.php
+++ app/includes/bottommenu.php
@@ -1,12 +1,33 @@
 <section class="nav bottom">
+
+	<nav>
+		<ul>
+			<li>
+				<a href="">
+					<span class="screenup">Settings</span>
+				</a>
+			</li><li class="questmaster-wrapper">&nbsp;
+				<a href="" class="questmaster">
+					<img src="images/questmaster_still.png">
+				</a>
+			</li><li>
+				<a href="">
+					Social
+				</a>
+			</li>
+		</ul>
+	</nav>
+<!--
 	<div class="bottomBtn">
-		<span class="screenup">Settings</span>
+					<span class="screenup">Settings</span>
 	</div>
 	<div class="bottomBtn questmaster">
 		<!--<img src="images/questmaster_still.png">-->
+		<!--
 	</div>
 	<div class="bottomBtn">
 		<span class="socialbtn"></span>
 		<img src="images/social_icon.png">
-	</div>
+	</div>-->
+	
 </section>
