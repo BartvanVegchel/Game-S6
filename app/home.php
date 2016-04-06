3f1ed8eaed22d37517e1c3b14ddd5705875ca618
Geert Molenaar <geert199@gmail.com>
2016-04-06 11:21:56 +0200

    Check if loggedin op home.php, anders redirect naar index.php

--- app/home.php
+++ app/home.php
@@ -1,22 +1,32 @@
 <? 
+if(isset($_SESSION["logged_in"])){ //controleer of je bent ingelogd
 	include('includes/header.php');
 	include('functions/login_function.php');
 	
 	include 'includes/topmenu.php';
 	include 'includes/bottommenu.php';
-?>
 
-<section class="socialScreenUp">
-	<? include 'modules/screenover/social.php'; ?>
-</section>
+	
+	?>
 
-<section class="screenOver slideUp">
-<!--	<h1>Schermen van onder naar beneden</h1>-->
-</section>
+		<section class="socialScreenUp">
+			<? include 'modules/screenover/social.php'; ?>
+		</section>
 
-<!-- include the map -->
-<? include 'modules/map/map.php'; ?>
+		<section class="screenOver slideUp">
+		<!--	<h1>Schermen van onder naar beneden</h1>-->
+		</section>
+
+		<!-- include the map -->
+		<? include 'modules/map/map.php'; ?>
+
 
 <? 
 include 'includes/footer.php'; 
+
+
+		}
+	else {
+		header("Refresh: 0; url=index.php");
+	}
 ?>
