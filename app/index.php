1ec5b3ff5d0c4685df7cbd93f31246a174d47c2c
Geert Molenaar <geert199@gmail.com>
2016-04-08 15:56:22 +0200

    Login en Register page edit

--- app/index.php
+++ app/index.php
@@ -4,19 +4,48 @@
 ?>
 
 <section class="login">
-	<h1>Inloggen</h1>
-	
-	<div class="">
-	</div>
 
-	<?php login(); ?>
-    <form method="POST" action="" id="register">
-        <input type="text" name="username" id="username" placeholder="Gebruikersnaam" tabindex="1">
-        <input type="password" name="password" id="password" placeholder="Wachtwoord" tabindex="5">
-        <input class="btn" type="submit" name="submit" value="Inloggen">
-    </form>
-    
-    <a href="register.php">Nieuw bij deze app? Registreer je dan hier!</a>
+	<div class="wrapper">
+	
+		<!--
+		<div class="column column-primary">
+		
+			<div class="column-header">
+				<h1>Welkom bij Moving Monsters!</h1>
+			</div>
+			
+			<div class="column-body">
+				<p>Beweeg, ontdek en verzamel de leukste monsters</p>
+			</div>
+		
+			
+		</div>
+		-->
+		
+		<div class="column">
+		
+			<div class="column-header">
+				<img src="images/monster_droplet.png" />
+				<h1>Wil je mij je naam vertellen?</h1>
+			</div>
+			
+			<div class="column-body">
+			
+				<?php login(); ?>
+				<form method="POST" action="" id="register">
+					<input type="text" name="username" id="username" placeholder="Typ je naam hier" tabindex="1">
+					<input type="password" name="password" id="password" placeholder="En hier je wachtwoord" tabindex="5">
+					<input class="btn" type="submit" name="submit" value="Laat me spelen!">
+				</form>
+				
+				<a class="btn btn-secondary btn-small" href="register.php">Ik ben een nieuwe speler</a>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
 </section>
 
 <? include 'includes/footer.php'; ?>
--- app/register.php
+++ app/register.php
@@ -4,20 +4,42 @@
 ?>
 
 <section class="register">
-	<a href="index.php">Terug naar inloggen</a>
-	<h1>Registreren</h1>
 
-	<?php register(); ?>
-    <form method="POST" action="" id="register">
-        <input type="text" name="username" id="username" placeholder="Gebruikersnaam" tabindex="1">
-        <input type="email" name="email" id="email" placeholder="E-mail" tabindex="2">
-        <label for="jongen">Jongen</label><input type="radio" name="gender" id="jongen" value="Jongen" tabindex="3">
-        <label for="meisje">Meisje</label><input type="radio" name="gender" id="meisje" value="Meisje" tabindex="4">
-        <input type="password" name="password" id="password" placeholder="Wachtwoord" tabindex="6">
-        <input type="password" name="password2" id="password2" placeholder="Wachtwoord herhaling" tabindex="7">
-        
-        <input class="btn" type="submit" name="submit" value="Registreren!">
-    </form>
+	<div class="wrapper">
+		
+		<div class="column">
+		
+			<div class="column-header">
+				<img src="images/monster_leefy.png" />
+				<h1>Wil je mij je naam vertellen?</h1>
+			</div>
+			
+			<div class="column-body">
+
+				<?php register(); ?>
+				<form method="POST" action="" id="register">
+					<input type="text" name="username" id="username" placeholder="Typ hier je naam" tabindex="1">
+					<input type="email" name="email" id="email" placeholder="Typ hier je e-mailadres" tabindex="2">
+					<div class="radio-wrapper">
+						<label for="jongen">Ik ben een jongen</label>
+						<input type="radio" name="gender" id="jongen" value="Jongen" tabindex="3">
+					</div><div class="radio-wrapper">
+						<label for="meisje">Ik ben een meisje</label>
+						<input type="radio" name="gender" id="meisje" value="Meisje" tabindex="4">
+					</div>
+					<input type="password" name="password" id="password" placeholder="Kies hier je wachtwoord" tabindex="6">
+					<input type="password" name="password2" id="password2" placeholder="Typ hier je wachtwoord nog een keer" tabindex="7">
+					
+					<input class="btn" type="submit" name="submit" value="Laat me nu spelen!">
+				</form>
+				
+				<a class="btn btn-secondary btn-small" href="index.php">Ik heb al eerder gespeeld</a>
+				
+			</div>
+			
+		</div>
+		
+	</div>
 
 </section>
 
