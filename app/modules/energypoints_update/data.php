6d801e3851b3135777fd0a3fafcb1a95559a2fe4
aarnoo92 <arnopsv@hotmail.com>
2016-04-09 16:50:45 +0200

    Stappenteller test5

--- app/modules/energypoints_update/data.php
+++ app/modules/energypoints_update/data.php
@@ -66,7 +66,7 @@ if (isset($_GET['code'])) {
     echo '<br>huidige stappenDB is' . $currentStepAmount;
     echo '<br>nieuwe stappen is' . $stepAmount;
 
-    header("Refresh: 0; url=../../home.php");
+    header("Refresh: 4; url=../../home.php");
 }
 
 
