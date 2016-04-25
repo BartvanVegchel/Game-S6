80c93ca362b72cc12ad6b207a63d820d3cb58af8
aarnoo92 <arnopsv@hotmail.com>
2016-04-25 16:07:07 +0200

    buildMap function included

--- phonegap/MovingMonsters/www/js/functions.js
+++ phonegap/MovingMonsters/www/js/functions.js
@@ -1,27 +1,50 @@
 $userId = localStorage.getItem('userInfo');
+$worldId = 2;
+
 console.log('functions ingeladen voor ' + $userId);
 
 function getEnergypoints(){
     var dataString="username="+$userId+"&submit=";
-
-    $.ajax({
-        type:"POST",
-        url:"http://game.onlineops.nl/phonegap_php/getEnergypoints.php",
-        data: dataString,
-        crossDomain: true,
-        cache: false,
-        dataType: 'json',
-        success: function(data){
-            if(data['error']=="error")
-            {// if register is succesfull
-                alert("error");
-                $(".personalEnergypoints").html(0);
-            } else if(data['energypoints'] !== ""){
-                $(".personalEnergypoints").html(data['energypoints']);
+    if(localStorage.getItem('userInfo') !== null) {
+        $.ajax({
+            type: "POST",
+            url: "http://game.onlineops.nl/phonegap_php/getEnergypoints.php",
+            data: dataString,
+            crossDomain: true,
+            cache: false,
+            dataType: 'json',
+            success: function (data) {
+                if (data['error'] == "error") {// if register is succesfull
+                    alert("error");
+                    $(".personalEnergypoints").html(0);
+                } else if (data['energypoints'] !== "") {
+                    $(".personalEnergypoints").html(data['energypoints']);
+                }
+            },
+            error: function () {
+                alert("Er gaat iets verkeerd, neem contact met ons op!");
             }
-        },
-        error: function () {
-            alert("Er gaat iets verkeerd, neem contact met ons op");
-        }
-    })
+        })
+    }
+}
+
+function buildMap(){
+    var dataString="worldid="+$worldId+"&username="+$userId+"&submit=";
+    if(localStorage.getItem('userInfo') !== null) {
+        $.ajax({
+            type: "POST",
+            url: "http://game.onlineops.nl/phonegap_php/buildMap.php",
+            data: dataString,
+            crossDomain: true,
+            cache: false,
+            success: function (data) {
+                console.log(data);
+                $(".map-container").html(data);
+
+            },
+            error: function () {
+                alert("Er gaat iets verkeerd, neem contact met ons op!");
+            }
+        })
+    }
 }
