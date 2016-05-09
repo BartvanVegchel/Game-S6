5d6b8e68b64b230752e33696906a9f0bae5d8416
BartvanVegchel <bart.vanvegchel@hotmail.com>
2016-05-09 12:28:53 +0200

    Monsterchallenge pagina vernieuwd

--- phonegap/MovingMonsters/www/js/functions.js
+++ phonegap/MovingMonsters/www/js/functions.js
@@ -269,3 +269,32 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
 //open submenu
 
+function Countdown(options) {
+    var timer,
+        instance = this,
+        seconds = options.seconds || 10,
+        updateStatus = options.onUpdateStatus || function () {},
+        counterEnd = options.onCounterEnd || function () {};
+
+    function decrementCounter() {
+        updateStatus(seconds);
+        if (seconds === 0) {
+            counterEnd();
+            instance.stop();
+        }
+        seconds--;
+    }
+
+    this.start = function () {
+        clearInterval(timer);
+        timer = 0;
+        seconds = options.seconds;
+        timer = setInterval(decrementCounter, 1000);
+    };
+
+    this.stop = function () {
+        clearInterval(timer);
+    };
+}
+
+
