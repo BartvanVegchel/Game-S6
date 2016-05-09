f66b6596b2b605a038a321d9190b13f8938c81fb
Geert Molenaar <geert199@gmail.com>
2016-05-09 12:29:45 +0200

    Social - Comment functie social berichten

---
+++ social/send_comment_function.php
@@ -0,0 +1,45 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+	include('header.php');
+	include('includes/db_social.php');
+?>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // Controleer of het formulier verzonden is
+
+	if (!empty($_POST['postId']) &&
+		!empty($_POST['userPostId']) &&
+		!empty($_POST['userCommentId']) &&
+        !empty($_POST['userCommentName']) &&
+		!empty($_POST['comment'])) { // Controleer of benodigde velden wel ingevuld zijn
+
+		$postId 		  = mysqli_real_escape_string($db_social, $_POST["postId"]);
+		$userPostId       = mysqli_real_escape_string($db_social, $_POST["userPostId"]);
+		$userCommentId    = mysqli_real_escape_string($db_social, $_POST["userCommentId"]);
+		$userCommentName  = mysqli_real_escape_string($db_social, $_POST["userCommentName"]);
+		$comment          = mysqli_real_escape_string($db_social, $_POST["comment"]);
+		$postDate  		  = date('Y/m/d', time());
+		
+		//echo $postId . $userPostId . $userCommentId . $userCommentName . $comment . $postDate;
+
+		mysqli_query($db_social, "INSERT INTO socialComments (id, postId, userPostId, userCommentId, userCommentName, comment, postDate)
+							VALUES ('', '$postId', '$userPostId', '$userCommentId', '$userCommentName', '$comment', '$postDate') ");
+	
+		header("Refresh: 3; url=index.php");
+		echo "<div class='container'><div class='alert alert-success'><strong><span class=\"entypo-check\"></span>Your comment has been created</strong>" . 
+		"<p>Congratulations! Your comment has been created.</p></div></div>"; // als het succesvol naar de database is geplaatst
+	}
+	else {
+		echo "<div class='container'><div class='alert alert-warning'>Kut, het werkt niet!</div></div>";
+	}
+			
+}
+    
+?>
+
+<?php
+    include('footer.php');
+?>
--- social/social_item.php
+++ social/social_item.php
@@ -24,6 +24,11 @@
 							$category = $rij["category"];
 							$postDate = $rij["postDate"];
 							$image = $rij["image"];
+							
+							$postDate1 = substr($postDate, -2);
+							$postDate2 = substr($postDate, -5, -3);
+							$postDate3 = substr($postDate, 0, 4);
+							$postDate = $postDate1 . "-" . $postDate2 . "-" . $postDate3;
 							?>
 							
 							<article class="col-xs-12 col-md-12">
@@ -40,44 +45,86 @@
 									</div>
 								</div>
 							</article>
-
-							<div class="col-xs-12 col-md-12">
-								<form method="POST" action="send_comment_function.php" style="margin-bottom: 20px;">
-									<input type="hidden" name="postId" value="<?php echo $postId; ?>" />
-									<input type="hidden" name="userPostId" value="<?php echo $userId; ?>" />
-									<input type="hidden" name="userCommentId" value="<?php echo ''; ?>" />
-									<input type="hidden" name="userCommentName" value="<?php echo $_SESSION['username']; ?>" />
-									<textarea class="form-control" placeholder="Reageren" ></textarea>
-									<input class="btn btn-primary" type="submit" name="submit" value="Plaats reactie" />
-								</form>
-							</div>
 							
-							<div class="col-xs-12 col-md-12">
-								<div class="row">
-									<div class="col-md-2 col-sm-2 hidden-xs">
-										<figure class="thumbnail">
-											<img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
-											<figcaption class="text-center">username</figcaption>
-										</figure>
+							<?php
+								if(isset($_SESSION["logged_in"])){ //controleer of je bent ingelogd
+								
+									$username = $_SESSION['username'];
+									
+									$getUserCommentId = mysqli_query($db, "SELECT * FROM users WHERE username = '$username' ") or die("FOUT: " . mysqli_error($dblink));
+								
+									while($rij = mysqli_fetch_assoc($getUserCommentId)) {
+										$userCommentId = $rij["id"];
+									}
+							?>
+
+									<div class="col-xs-12 col-md-12">
+										<form method="POST" action="send_comment_function.php" style="margin-bottom: 20px;">
+											<input type="hidden" name="postId" value="<?php echo $postId; ?>" />
+											<input type="hidden" name="userPostId" value="<?php echo $userId; ?>" />
+											<input type="hidden" name="userCommentId" value="<?php echo $userCommentId; ?>" />
+											<input type="hidden" name="userCommentName" value="<?php echo $_SESSION['username']; ?>" />
+											<textarea class="form-control" placeholder="Reageren" name="comment" ></textarea>
+											<input class="btn btn-primary" type="submit" name="submit" value="Plaats reactie" />
+										</form>
 									</div>
-									<div class="col-md-10 col-sm-10">
-										<div class="panel panel-default arrow left">
-											<div class="panel-body">
-												<header class="text-left">
-													<div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
-													<time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
-												</header>
-												<div class="comment-post">
-													<p>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-													</p>
+									
+							<?php
+								}
+								else {
+								}
+							?>
+							
+							<?php
+							
+								$getSocialComments = mysqli_query($db_social, "SELECT * FROM socialComments WHERE postId = '$socialId' ") or die("FOUT: " . mysqli_error($dblink));
+								
+								while($rij = mysqli_fetch_assoc($getSocialComments)) {
+									$commentId = $rij["id"];
+									$postId = $rij["postId"];
+									$userPostId = $rij["userPostId"];
+									$userCommentId = $rij["userCommentId"];
+									$userCommentName = $rij["userCommentName"];
+									$comment = $rij["comment"];
+									$postDate = $rij["postDate"];
+							
+									$postDate1 = substr($postDate, -2);
+									$postDate2 = substr($postDate, -5, -3);
+									$postDate3 = substr($postDate, 0, 4);
+									$postDate = $postDate1 . "-" . $postDate2 . "-" . $postDate3;
+									?>
+									
+									<div class="col-xs-12 col-md-12">
+										<div class="row">
+											<div class="col-md-2 col-sm-2 hidden-xs">
+												<figure class="thumbnail">
+													<img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+													<figcaption class="text-center"><?php echo $userCommentName; ?></figcaption>
+												</figure>
+											</div>
+											<div class="col-md-10 col-sm-10">
+												<div class="panel panel-default arrow left">
+													<div class="panel-body">
+														<header class="text-left">
+															<time class="comment-date"><i class="fa fa-clock-o"></i> <?php echo $postDate; ?></time>
+														</header>
+														<div class="comment-post">
+															<p>
+																<?php echo $comment; ?>
+															</p>
+														</div>
+													</div>
 												</div>
-												<p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
 											</div>
 										</div>
 									</div>
-								</div>
-							</div>
+									
+									<?php
+								}
+							
+							?>
+								
+								
 							
 						<?php
 							
