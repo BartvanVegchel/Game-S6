c38e5451f49422a38d997fac6932934090f882c7
Geert Molenaar <geert199@gmail.com>
2016-05-12 09:25:52 +0200

    Social - Comment functie blogposts + css fix

--- social/blog_item.php
+++ social/blog_item.php
@@ -20,22 +20,106 @@ if($blogId != ''){
 			$postDate = $rij["postDate"];
 			$image = $rij["image"];
 			?>
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-2 col-md-8">
 			
-			<article class="col-wi-1 center-block">
-				<div class="panel panel-default blog_page">
-					<div class="panel-heading">
-						<?php echo "<img src='./" . $image . "' height='200px' width='100%' alt='#' />"; ?>
+					<article class="col-xs-12">
+						<div class="panel panel-default blog_page">
+							<div class="panel-heading">
+								<?php echo "<img src='./" . $image . "' height='200px' width='100%' alt='#' />"; ?>
+							</div>
+							<div class="panel-body">
+								<h1><?php echo $title; ?></h1>
+								<p><?php echo $content; ?></p>
+							</div>
+							<div class="panel-footer">
+								</span><span class="post_date"><?php echo $postDate; ?></span>
+								<div class="clear"></div>
+							</div>
+						</div>
+					</article>
+			
+			
+			<?php
+				if(isset($_SESSION["logged_in"])){ //controleer of je bent ingelogd
+				
+					$username = $_SESSION['username'];
+					
+					$getUserCommentId = mysqli_query($db, "SELECT * FROM users WHERE username = '$username' ") or die("FOUT: " . mysqli_error($dblink));
+				
+					while($rij = mysqli_fetch_assoc($getUserCommentId)) {
+						$userCommentId = $rij["id"];
+					}
+			?>
+
+					<div class="col-xs-12 col-md-12">
+						<form method="POST" action="send_blogcomment_function.php" style="margin-bottom: 20px;">
+							<input type="hidden" name="postId" value="<?php echo $postId; ?>" />
+							<input type="hidden" name="userPostId" value="<?php echo $userId; ?>" />
+							<input type="hidden" name="userCommentId" value="<?php echo $userCommentId; ?>" />
+							<input type="hidden" name="userCommentName" value="<?php echo $_SESSION['username']; ?>" />
+							<textarea class="form-control" placeholder="Reageren" name="comment" ></textarea>
+							<input class="btn btn-primary" type="submit" name="submit" value="Plaats reactie" />
+						</form>
 					</div>
-					<div class="panel-body">
-						<h1><?php echo $title; ?></h1>
-						<p><?php echo $content; ?></p>
+					
+			<?php
+				}
+				else {
+				}
+			?>
+			
+			<?php
+			
+				$getBlogComments = mysqli_query($db_social, "SELECT * FROM blogComments WHERE postId = '$blogId' ") or die("FOUT: " . mysqli_error($dblink));
+				
+				while($rij = mysqli_fetch_assoc($getBlogComments)) {
+					$commentId = $rij["id"];
+					$postId = $rij["postId"];
+					$userPostId = $rij["userPostId"];
+					$userCommentId = $rij["userCommentId"];
+					$userCommentName = $rij["userCommentName"];
+					$comment = $rij["comment"];
+					$postDate = $rij["postDate"];
+			
+					$postDate1 = substr($postDate, -2);
+					$postDate2 = substr($postDate, -5, -3);
+					$postDate3 = substr($postDate, 0, 4);
+					$postDate = $postDate1 . "-" . $postDate2 . "-" . $postDate3;
+					?>
+					
+					<div class="col-xs-12 col-md-12">
+						<div class="row">
+							<div class="col-md-2 col-sm-2 hidden-xs">
+								<figure class="thumbnail">
+									<img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+									<figcaption class="text-center"><?php echo $userCommentName; ?></figcaption>
+								</figure>
+							</div>
+							<div class="col-md-10 col-sm-10">
+								<div class="panel panel-default arrow left">
+									<div class="panel-body">
+										<header class="text-left">
+											<time class="comment-date"><i class="fa fa-clock-o"></i> <?php echo $postDate; ?></time>
+										</header>
+										<div class="comment-post">
+											<p>
+												<?php echo $comment; ?>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-					<div class="panel-footer">
-						</span><span class="post_date"><?php echo $postDate; ?></span>
-						<div class="clear"></div>
-					</div>
+					
+					<?php
+				}
+			
+			?>
+			
 				</div>
-			</article>
+			</div>
 			
 		<?php
 			
