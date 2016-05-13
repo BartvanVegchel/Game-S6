8330f8c06f66e06d460bfe4555eab64aa605fe6f
Fraank92 <Snoeppot1!>
2016-05-13 15:21:24 +0200

    Style

--- promo/index.php
+++ promo/index.php
@@ -1,10 +1,61 @@
 <?php include('header.php'); ?>
 
-<div class="page-header">
-	<div class="background_img">
+<div class="page_intro">
+
+	<div class="playbtn" data-toggle="modal" data-target="#exampleModal" >
+		<img src="img/play-button.png" alt="Play video" />
+	</div>
+
+	<div class="intro_bg">
 		<img src="img/header_bg.png" alt="" />
 	</div>
 </div>
+
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
+			<div class="modal-body">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/QB3ry-04G-I" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="page_explore">
+
+	<div class="row">
+		<div class="col-xs-12 col-md-5">
+
+			<div class="explore_text">
+				<h2>Verken nieuwe gebieden en vang zo veel mogelijk monsters!</h2>
+				<p>Verken zo veel mogelijk gebieden en vang alle monsters die je tegenkomt!</p>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-md-5">
+
+			<div class="explore_img">
+				foto
+			</div>
+		</div>
+	</div><!--/row-->
+
+	<div class="explore_bg">
+		<img src="img/explore_bg.png" alt="" />
+	</div>
+
+</div>
+
+
+
+
+
 
 <div class="container">
 	<div class="row">
