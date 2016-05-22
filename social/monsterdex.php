6f925a9f6b0811968e44aebb6be2776be864067f
Minzo <bramvandinteren@hotmail.com>
2016-05-22 16:08:24 +0200

    update

--- social/monsterdex.php
+++ social/monsterdex.php
@@ -6,22 +6,20 @@
 				<div class="grass-section">
 					<h1 class="section-title">Forest Field</h1>
 
-
 					<div class="wow fadeInDown">
 						<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 							<img class="popuptext monsterimg" title='Leefy houdt maar van twee dingen; spelen en eten.
-						Dit monsters is misschien niet het slimste, maar hij zal er altijd zijn om je op te vrolijken!' id="leefy" src="img/monsterdex/leefy.png">
+						Dit monsters is misschien niet het slimste, maar hij zal er altijd zijn om je op te vrolijken!' id="leefy" src="img/monsterdex/leefy_img.png">
 							</img>
-						<h3>leefy</h3>
 						</section>
 						<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 							<img class="popuptext monsterimg" title="De overgrootouders van Shroom waren de eerste levende paddestoelen.
 						Zijn soort is zeldzaam en Shroom is de enigste die er is.
-						Rondspringen doet hij graag maar kijk wel uit voor zijn stekelige hoofd!" id="shroom" src="img/monsterdex/shroom.png">
+						Rondspringen doet hij graag maar kijk wel uit voor zijn stekelige hoofd!" id="shroom" src="img/monsterdex/shroom_img.png">
 							</img>
 						</section>
 						<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
-							<img class="popuptext monsterimg" title="De baas van het bos, Grasilla, waakt al eeuwen over Forest Field. Het gerucht gaat rond dat hij nog nooit in zijn leven heeft gelachen..." id="grasilla" src="img/monsterdex/grasilla.png">
+							<img class="popuptext monsterimg" title="De baas van het bos, Grasilla, waakt al eeuwen over Forest Field. Het gerucht gaat rond dat hij nog nooit in zijn leven heeft gelachen..." id="grasilla" src="img/monsterdex/grasilla_img.png">
 							</img>
 						</section>
 					</div>
@@ -57,17 +55,17 @@
 								<div class="wow fadeInDown" >
 									<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 										<img class="popuptext monsterimg" title="Deze vrolijke inktvis is zelden in een slechte bui en altijd opzoek naar een avontuur.
-									Met zijn scherpe tanden en zware tentakels is hij tevens ook een van de gevaarlijkste vissen, zonder dat hij dat zelf doorheeft." id="droplet" src="img/monsterdex/droplet.png">
+									Met zijn scherpe tanden en zware tentakels is hij tevens ook een van de gevaarlijkste vissen, zonder dat hij dat zelf doorheeft." id="droplet" src="img/monsterdex/droplet_img.png">
 										</img>
 									</section>
 									<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 										<img class="popuptext monsterimg" title="Deze zeester is de snelste vis van de hele oceaan.
-									Laat je niet afleiden door zijn schattigheid, want Twistar is gevaarlijker dan hij eruit ziet!" id="twistar" src="img/monsterdex/twistar.png">
+									Laat je niet afleiden door zijn schattigheid, want Twistar is gevaarlijker dan hij eruit ziet!" id="twistar" src="img/monsterdex/twistar_img.png">
 										</img>
 									</section>
 									<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 										<img class="popuptext monsterimg" title="In de diepste bodem van de oceaan woont Koral. Hier leeft hij een eenzaam bestaan, want zijn ouders zijn lang geleden overleden.
-									Zijn neefje droplet is de enigste familie die hij nog heeft, maar die heeft hij al jaren niet meer gezien." id="koral" src="img/monsterdex/koral.png">
+									Zijn neefje droplet is de enigste familie die hij nog heeft, maar die heeft hij al jaren niet meer gezien." id="koral" src="img/monsterdex/koral_img.png">
 										</img>
 									</section>
 								</div>
@@ -101,17 +99,17 @@
 						<div class="wow fadeInDown">
 							<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 										<img class="popuptext monsterimg" id="fury" title="Fury is het coolste monster in het universum.
-								Maar pas op, dit monster heeft een kort-lontje en is licht-ontvlambaar!" src="img/monsterdex/fury.png">
+								Maar pas op, dit monster heeft een kort-lontje en is licht-ontvlambaar!" src="img/monsterdex/fury_img.png">
 										</img>
 							</section>
 							<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 										<img class="popuptext monsterimg" id="boomer" title="Boomer, de rode duivel, heeft een passie voor alles dat slecht is.
-								Het enigste goede in hem is de liefde voor zijn broertje Flame, omdat die bijna net zo gemeen is als Boomer." src="img/monsterdex/boomer.png">
+								Het enigste goede in hem is de liefde voor zijn broertje Flame, omdat die bijna net zo gemeen is als Boomer." src="img/monsterdex/boomer_img.png">
 										</img>
 							</section>
 							<section id="img-section" class="col-lg-4 col-sm-4 col-xs-12">
 										<img class="popuptext monsterimg" id="magmo" title="Uit de lava van Volcano Islands is het monster Magmo ontstaan.
-								Er is verder niet veel bekend over dit monster, omdat hij maar een paar keer per jaar uit zijn vulkaan komt." src="img/monsterdex/magmo.png">
+								Er is verder niet veel bekend over dit monster, omdat hij maar een paar keer per jaar uit zijn vulkaan komt." src="img/monsterdex/magmo_img.png">
 										</img>
 							</section>
 						</div>
@@ -139,7 +137,7 @@
 				<section class="col-lg-12">
 					<div class="desert-section">
 						<h1 class="section-title">Desert Valley</h1>
-						<h2>Nog eventjes wachten!</h2>
+						<h2 class="comingsoon">Nog eventjes wachten!</h2>
 					</div>
 				</section>
 			</div>
@@ -147,7 +145,7 @@
 				<section class="col-lg-12">
 					<div class="ice-section">
 						<h1 class="section-title">Ice Cave</h1>
-						<h2>Nog eventjes wachten!</h2>
+						<h2 class="comingsoon">Nog eventjes wachten!</h2>
 					</div>
 				</section>
 			</div>
@@ -159,59 +157,30 @@
 	$(document).ready(function(){
 
 			/* ---- TOOLTIP ---- */
-			var options = {
+			var options1 = {
 				width: '200px',
-				trigger: 'click',
+				trigger: 'mouseenter',
 				pointer: 'left',
 				closeOnClick: true,
-				closeButton: 'box',
 				animation: 'zoomOut'
 
 			};
 
-			$('.popuptext').jBox('Tooltip', options);
+		var options2 = {
+			width: '200px',
+			trigger: 'click',
+			pointer: 'left',
+			closeOnClick: true,
+			animation: 'zoomOut'
 
+		};
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$('.popuptext').jBox('Tooltip', options2);
+		}
+		else {
+			$('.popuptext').jBox('Tooltip', options1);
+		}
 
-		/* ---- Selected Monster ---- */
-
-		var clicks = 0;
-
-		$('#leefy').click(function(){
-			if(clicks == 0) {
-				$("#leefy").attr('src', "img/monsterdex/leefy_hover.png");
-				clicks++;
-				console.log(clicks);
-			}
-			else if(clicks == 1){
-				$("#leefy").attr('src', "img/monsterdex/leefy.png");
-				clicks--;
-				console.log(clicks);
-			}
-		});
-		$('#shroom').click(function(){
-			if(clicks == 0) {
-				$("#shroom").attr('src', "img/monsterdex/shroom_hover1.png");
-				clicks++;
-				console.log(clicks);
-			}
-			else if(clicks == 1){
-				$("#shroom").attr('src', "img/monsterdex/shroom.png");
-				clicks--;
-				console.log(clicks);
-			}
-		});
-		$('#grasilla').click(function(){
-			if(clicks == 0) {
-				$("#grasilla").attr('src', "img/monsterdex/grasilla_hover.png");
-				clicks++;
-				console.log(clicks);
-			}
-			else if(clicks == 1){
-				$("#grasilla").attr('src', "img/monsterdex/grasilla.png");
-				clicks--;
-				console.log(clicks);
-			}
-		});
 
 		$("#droplet-info").hide();
 		$("#twistar-info").hide();
