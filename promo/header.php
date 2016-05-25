f9167e6f86e45c88a61a4dbafe63debb5a9e87a7
Geert Molenaar <geert199@gmail.com>
2016-05-25 09:42:35 +0200

    Promo - container fluid delete

--- promo/header.php
+++ promo/header.php
@@ -47,7 +47,6 @@
 
 <header>
     <nav class="navbar navbar-default navbar-inverse" role="navigation">
-        <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -120,6 +119,5 @@
 					?>
                 </ul>
             </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
     </nav>
 </header>
